fb8ca6684f3d411cf13671ee68eefc8c5c0d127e
NomeUn <benjamin.rondeau.pro@gmail.com>
2023-02-13 17:19:03 +0100

    Création de l'interface pour l'affichage de la météo

---
+++ app/src/main/java/com/example/Meteo/AffichageMeteo.kt
@@ -0,0 +1,12 @@
+package com.example.Meteo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.applicationmeteo.R
+
+class AffichageMeteo : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_affichage_meteo)
+    }
+}
--- app/src/main/java/com/example/applicationmeteo/MainActivity.kt
+++ app/src/main/java/com/example/applicationmeteo/MainActivity.kt
@@ -1,11 +1,28 @@
 package com.example.applicationmeteo
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.example.Meteo.AffichageMeteo
 
 class MainActivity : AppCompatActivity() {
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnConsultationMeteo = findViewById<Button>(R.id.btnConsultationMeteo)
+
+        btnConsultationMeteo.setOnClickListener{
+            Intent(this, AffichageMeteo::class.java).also {
+                startActivity(it)
+            }
+        }
+
     }
+
+
 }
